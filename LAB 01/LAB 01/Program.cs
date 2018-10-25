1784a2ca0c68c190c390aa015e27986e38835c3d
IT1050-AlyssaFresso <alyssa.ekstrand@gmail.com>
2018-10-24 21:22:12 -0700

    Lab 01 complete

---
+++ LAB 01/LAB 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldfromAlyssaFresso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Alyssa Fresso");
+            Console.ReadLine();
+
+        }
+    }
+}
